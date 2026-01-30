384f790050a38a76f8a0140e16b5554a0fb692cd
Ivan <vaniasidorenko001@gmail.com>
2026-01-30 14:05:06 +0300

    Fix

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react';
 import { setOptions, importLibrary } from '@googlemaps/js-api-loader';
-import styles from '../styles/CityBlocks.module.css';
+import styles from '@/styles/CityBlocks.module.css';
 
 const GoogleMap = () => {
   const mapRef = useRef(null);
